fa1f8a9d48eb43ed4560bb0e333828b19d40ac99
LeonardoFate <alvillamarf@gmail.com>
2024-11-07 17:53:16 -0500

    filtro y sort agregado

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,50 +1,48 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-
-
-
-
-
-export interface PeriodicElement {
-    name: string;
-    position: number;
-    weight: number;
-    symbol: string;
-  }
-
-  const ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-    {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-    {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-    {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-    {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-    {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-    {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-    {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-    {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-    {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-    {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-    
-  ];
+import {
+  AfterViewInit,
+  Component,
+  input,
+  OnInit,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInput } from '@angular/material/input';
+const MATERIAL_MODULES = [
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatPaginatorModule,
+  MatLabel,
+  MatFormField, MatInput
+];
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule],
+  imports: [MATERIAL_MODULES],
   templateUrl: './grid.component.html',
-  styleUrl: './grid.component.scss'
+  styleUrl: './grid.component.scss',
 })
-export class GridComponent {
-    displayedColumns = ['position', 'name', 'weight', 'symbol'];
-    dataSource = ELEMENT_DATA;
+export class GridComponent<T> implements OnInit {
+  displayedColumns = input.required<string[]>();
+  data = input.required<T[]>();
+
+  dataSource = new MatTableDataSource<T>();
+  private readonly _sort = viewChild.required<MatSort>(MatSort);
+  private readonly _paginator = viewChild.required<MatPaginator>(MatPaginator);
+
+  ngOnInit(): void {
+    this.dataSource.data = this.data();
+    this.dataSource.sort = this._sort();
+    this.dataSource.paginator = this._paginator();
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
